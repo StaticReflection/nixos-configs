43bee7bf348a13eb9c86affea6dcc4fae2df4792
StaticReflection <deng3616888303@gmail.com>
2025-11-02 20:00:00 +0800

    fix: netease settings page crash

--- home/reflexia/programs/lutris.nix
+++ home/reflexia/programs/lutris.nix
@@ -5,11 +5,14 @@
     (lutris.override {
       extraPkgs = pkgs: [
         # List package dependencies here
-        wineWow64Packages.full
+        wineWow64Packages.stagingFull
+        wineWowPackages.wayland
+        wineWowPackages.minimal
       ];
       extraLibraries = pkgs: [
         # List library dependencies here
         wineWow64Packages.fonts
+        wineWowPackages.fonts
       ];
     })
   ];
