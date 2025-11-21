434368fc6c283da3c9b2842ed01784aab34e36eb
StaticReflection <deng3616888303@gmail.com>
2025-11-21 16:44:35 +0800

    feat: flutter 环境添加 web 运行

--- env/flutter.nix
+++ env/flutter.nix
@@ -4,4 +4,9 @@
   environment.systemPackages = with pkgs; [
     flutter # Flutter SDK
   ];
+
+  # Web
+  environment.variables = {
+    CHROME_EXECUTABLE = "${pkgs.google-chrome}/bin/google-chrome-stable";
+  };
 }
