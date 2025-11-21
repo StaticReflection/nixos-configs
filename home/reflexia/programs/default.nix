4586ac5b1449391358937f2af4e4ef4bd7f3f28a
StaticReflection <deng3616888303@gmail.com>
2025-11-21 16:25:03 +0800

    feat: 添加draw.io

--- home/reflexia/programs/default.nix
+++ home/reflexia/programs/default.nix
@@ -14,20 +14,22 @@
   ];
 
   home.packages = with pkgs; [
-    discord
-    telegram-desktop
-    microsoft-edge
-    prismlauncher
-    feishu
-    libreoffice-qt
-    bitwarden-desktop
-    _7zz-rar
-    kdePackages.kcalc
-    waydroid-helper
-    nur.repos.shadowrz.klassy-qt6
-    vlc
-    appimage-run
-    wechat-uos
-    qbittorrent-enhanced
+    discord # Discord
+    telegram-desktop # Telegram
+    microsoft-edge # Microsoft Edge
+    prismlauncher # Prism Launcher (Minecraft)
+    feishu # Feishu (Lark)
+    libreoffice-qt # LibreOffice with Qt5
+    bitwarden-desktop # Bitwarden
+    _7zz-rar # 7zz with RAR support
+    kdePackages.kcalc # KCalc 计算器
+    waydroid-helper # Waydroid helper script
+    nur.repos.shadowrz.klassy-qt6 # Klassy Qt6 theme
+    vlc # VLC media player
+    appimage-run # AppImage support
+    wechat-uos # WeChat UOS version
+    qbittorrent-enhanced # qBittorrent Enhanced Edition
+    hoppscotch # Hoppscotch
+    drawio # draw.io
   ];
 }
